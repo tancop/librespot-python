4348689aa08180309d28af90151e02cee172e35c
Teodor Potancok <teodor.potancok@gmail.com>
2025-05-28 18:14:23 +0200

    Remove useless password encode

--- librespot/core.py
+++ librespot/core.py
@@ -1639,7 +1639,7 @@ class Session(Closeable, MessageListener, SubListener):
             self.login_credentials = Authentication.LoginCredentials(
                 username=username,
                 auth_type=Authentication.AuthenticationType.AUTHENTICATION_USER_PASS,
-                auth_data=password.encode(),
+                auth_data=password,
             )
             return self
 
